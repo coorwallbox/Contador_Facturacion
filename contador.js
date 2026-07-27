7695978ea09822fd3dc626b84889fe8dbffa49e7
Coorwallbox1 <coorwallbox@loscoches.com.co>
2026-07-27 09:55:09 -0500

    actualizacion de ventas

--- contador.js
+++ contador.js
@@ -17,10 +17,10 @@ const DURACION = 3000;
 // se calcula como la suma de estas ventas.
 const VENDEDORES = [
     { nombre: "DISTRIBUIDORA LOS COCHES LA SABANA S.A.S", ventas: 70 },
-    { nombre: "carlos Alberto Martin",                    ventas: 5  },
+    { nombre: "carlos Alberto Martin",                    ventas: 8  },
     { nombre: "Andrés Felipe Saavedra Rojas",             ventas: 0  },
-    { nombre: "Isay Duban Laverde Cetina",                ventas: 0  },
-    { nombre: "Internos",                                 ventas: 0  }    
+    { nombre: "Isay Duban Laverde Cetina",                ventas: 1  },
+    { nombre: "Internos",                                 ventas: 10  }    
 ];
 
 /*==================================================
