b3d67e1950f5f129b0ac53819d9921500b35cbfc
Coorwallbox1 <coorwallbox@loscoches.com.co>
2026-08-25 16:03:49 -0500

    cambio de nombres

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ const DURACION = 3000;
 // El HTML se genera automáticamente y el total del contador
 // se calcula como la suma de estas ventas.
 const VENDEDORES = [
-    { nombre: "DISTRIBUIDORA LOS COCHES LA SABANA S.A.S", ventas: 50 },
+    { nombre: "Los Coches La Sabana",                     ventas: 50 },
     { nombre: "Carlos Alberto Martin",                    ventas: 0  },
     { nombre: "Andrés Felipe Saavedra Rojas",             ventas: 65  },
     { nombre: "Isay Duban Laverde Cetina",                ventas: 37  },
